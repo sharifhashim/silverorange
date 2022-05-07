196870ed16175a050a1dbb518637b3dc9449c447
Hashim Sharif <sharif.hashim@gmail.com>
2022-05-07 14:02:21 -0400

    destructure useApplicationData hook

--- web/src/components/TableList.jsx
+++ web/src/components/TableList.jsx
@@ -7,42 +7,46 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import useApplicationData from '../hooks/useApplicationData';
+import BasicButtons from './Buttons';
 
 export default function TableList() {
-  const { state } = useApplicationData();
-  const sorted = state
-    .sort((a, b) => {
-      return new Date(a.created_at) - new Date(b.created_at);
-    })
-    .reverse();
+  const { state, setState, stateCopy, setStateCopy } = useApplicationData();
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Description</TableCell>
-            <TableCell align="right">Language</TableCell>
-            <TableCell align="right">Forks Count</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {sorted.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.description}</TableCell>
-              <TableCell align="right">{row.language}</TableCell>
-              <TableCell align="right">{row.forks_count}</TableCell>
+    <div>
+      <BasicButtons
+        state={state}
+        setState={setState}
+        stateCopy={stateCopy}
+        setStateCopy={setStateCopy}
+      />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow sx={{ bgcolor: 'lightblue' }}>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Description</TableCell>
+              <TableCell align="right">Language</TableCell>
+              <TableCell align="right">Forks Count</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {stateCopy.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.description}</TableCell>
+                <TableCell align="right">{row.language}</TableCell>
+                <TableCell align="right">{row.forks_count}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
