acf761207f62e2064c11894c289e7b536f69bda9
Hashim Sharif <sharif.hashim@gmail.com>
2022-05-07 11:57:39 -0400

    aggregate repo data and return only repos where repository.fork is false

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,5 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, response } from 'express';
+import repo from '../../data/repos.json';
 const axios = require('axios');
 export const repos = Router();
 
@@ -6,10 +7,22 @@ repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
   axios
     .get('https://api.github.com/users/silverorange/repos')
-    .then((data: any) => res.json(data.data))
+    .then((data: any) => {
+      let response = [];
+      for (const arr of repo) {
+        if (arr.fork === false) {
+          response.push(arr);
+        }
+      }
+      for (const arr of data.data) {
+        if (arr.fork === false) {
+          response.push(arr);
+        }
+      }
+      res.send(response);
+    })
     .catch((err: any) => res.json(err));
   //res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  //res.json([]);
 });
