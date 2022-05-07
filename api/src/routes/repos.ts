2421a1395022ced07441f8ff31799a13040cd057
Hashim Sharif <sharif.hashim@gmail.com>
2022-05-07 15:00:36 -0400

    create route to display single repo

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -24,3 +24,26 @@ repos.get('/', async (_: Request, res: Response) => {
     })
     .catch((err: any) => res.json(err));
 });
+
+repos.get('/:name', (req, res) => {
+  res.header('Cache-Control', 'no-store');
+  const name = req.params.name;
+  axios
+    .get('https://api.github.com/users/silverorange/repos')
+    .then((data: any) => {
+      let response = [];
+      for (const arr of repo) {
+        if (arr.fork === false && arr.name === name) {
+          response.push(arr);
+        }
+      }
+      for (const arr of data.data) {
+        if (arr.fork === false && arr.name === name) {
+          response.push(arr);
+        }
+      }
+
+      res.status(200).send(response);
+    })
+    .catch((err: any) => res.json(err));
+});
